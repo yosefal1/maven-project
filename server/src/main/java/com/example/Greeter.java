76367d81ab98a68c5171cd8433ccaa1352ef56b1
yosefal1 <37108079+yosefal1@users.noreply.github.com>
2018-11-11 10:45:28 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
